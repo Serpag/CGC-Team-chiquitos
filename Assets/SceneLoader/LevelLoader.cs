ac226f6e21542844995d395f59e5c25fd3c81a68
kalucifer <muddokon@hotmail.es>
2020-10-08 13:36:06 -0500

    Scenes Ready
    Scenes updated
    linked ready
    unity 2020.1.8 updated

---
+++ Assets/SceneLoader/LevelLoader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public string nextScene;
+    public float timerAuto;
+
+    private void Start()
+    {
+        if(timerAuto!=0)
+            StartCoroutine(delayAuto());
+    }
+
+
+    public void ChageScene(string sceneName)
+    {
+        StartCoroutine(delay(sceneName));
+    }
+
+    IEnumerator delay(string sceneName)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(sceneName);
+    }
+
+
+    IEnumerator delayAuto()
+    {
+        yield return new WaitForSeconds(timerAuto);
+        StartCoroutine(delay(nextScene));
+    }
+}
