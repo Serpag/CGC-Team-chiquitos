e3f67dca99048707c0e7034381e3af252e6d8482
kalucifer <muddokon@hotmail.es>
2020-10-08 21:36:09 -0500

    Enemy Rana
    enemy ready

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+ using System.Collections.Generic;
+ using UnityEngine;
+ 
+ public class Projectile : MonoBehaviour {
+     
+     private Vector2 target;
+     public float speed;
+
+     Vector3 moveDirection;
+ 
+     void Start()
+     {
+         target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+ 
+         StartCoroutine(destroyAfterTime());
+         
+         moveDirection = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+          moveDirection.z = 0;       
+          moveDirection.Normalize();
+        
+     }
+ 
+     private void Update()
+     {
+         transform.position = transform.position + moveDirection * speed* Time.deltaTime;
+ 
+     }
+ 
+     
+ 
+     private void OnTriggerEnter2D(Collider2D other)
+     {
+         if (other.gameObject.tag == "Floor" )
+         {
+         Destroy(gameObject);
+
+         }
+     }
+ 
+ 
+     IEnumerator destroyAfterTime()
+     {
+         yield return new WaitForSeconds(2f);
+         Destroy(gameObject);
+     }
+ 
+ }
