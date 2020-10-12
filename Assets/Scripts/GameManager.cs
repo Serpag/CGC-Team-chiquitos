55ead4bb7d4009da90263012b3288c8522a47dfc
kalucifer <muddokon@hotmail.es>
2020-10-12 15:31:37 -0500

    Mechanics Ready
    Core Mechanics ready
    
    TODO
    Build up levels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,13 +1,19 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private LevelLoader _levelLoader;
+    public string nextLevel;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        ChangeLevel();
+    }
+
+    public void ChangeLevel()
+    {
+        _levelLoader.ChageScene(nextLevel);
     }
 
     // Update is called once per frame
